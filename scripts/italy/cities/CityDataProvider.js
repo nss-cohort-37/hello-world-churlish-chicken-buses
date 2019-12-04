8bc0b7302938d443da2951637fc13b4481d119cf
Case Scally <casescally@gmail.com>
2019-12-04 09:20:37 -0600

    pushing italy javascript automation

---
+++ scripts/italy/cities/CityDataProvider.js
@@ -0,0 +1,23 @@
+const citiesCollection = [
+    {
+        cityName: "Rome"
+    },
+    {
+        cityName: "Florence"
+    },
+    {
+        cityName: "Venice"
+    },
+    {
+        cityName: "Milan"
+    }, 
+    {
+        cityName: "Napels"
+    }, 
+]
+
+export const useCity = () => {
+    return citiesCollection
+}
+
+
---
+++ scripts/italy/leaders/LeaderDataProvider.js
@@ -0,0 +1,12 @@
+const leadersCollection = [
+    {
+        leaderName: "Sergio Mattarella - President"
+    },
+    {
+        leaderName: "Giuseppe Conte - Prime Minister of Italy"
+    }
+]
+
+export const useLeader = () => {
+    return leadersCollection
+}
