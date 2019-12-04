caa9245c87f6b6987d441ba3d1566b27e1a7b752
Case Scally <casescally@gmail.com>
2019-12-04 10:35:41 -0600

    added detail buttons

--- scripts/italy/cities/CitiesComponent.js
+++ scripts/italy/cities/CitiesComponent.js
@@ -1,7 +1,12 @@
 const CityComponent = (city) => {
-    
+
     return `
-<li class="listies--cities">${city.cityName}</li>
+        <li class="listies--cities">${city.cityName}</li>
+        <button id="city--${city.cityName}">Details</button>
+        <dialog class="dialog--city id="details--${city.cityName}">
+            <div>Population: ${city.population}</div>
+            <button class=button--close>Close Dialog</button>
+        </dialog>
     `
 }
 
--- scripts/italy/main.js
+++ scripts/italy/main.js
@@ -1,7 +1,9 @@
 import CityListComponent from "./cities/Cities.js"
 import LandmarkListComponent from "./Landmarks/Landmarks.js"
 import LeaderListComponent from "./leaders/Leaders.js"
+import initializeDetailButtonEvents from "./dialog.js"
 
 CityListComponent()
 LandmarkListComponent()
 LeaderListComponent()
+initializeDetailButtonEvents()
