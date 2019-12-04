62d2e80f5652fe60e32a03a691530650211af13f
rebeccapatek <rebeccapatek@gmail.com>
2019-12-04 11:32:27 -0600

    added landmark images

--- scripts/Vietnam/landmarks/LandmarksComponent.js
+++ scripts/Vietnam/landmarks/LandmarksComponent.js
@@ -1,6 +1,13 @@
 const LandMarksComponent = (landmark) => {
+    let LandmarkSpacelessName = landmark.landmarkName.split(" ").join("-")
     return `
     <li class="listies--landmarks">${landmark.landmarkName}</li>
+    <button id="city--${LandmarkSpacelessName}">Details</button>
+    <dialog class="dialog--landmark" id="details--${LandmarkSpacelessName}">
+        <img src="${landmark.landmarkImage}" alt="image of ${landmark.landmarkName}">
+
+    <button class="button--close">Close Dialog</button>
+    </dialog>
     `
 }
 export default LandMarksComponent
