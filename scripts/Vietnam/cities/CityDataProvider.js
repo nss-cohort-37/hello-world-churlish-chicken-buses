71b138df94c0d9cdcd0fd74fc88df7c81b87796f
rebeccapatek <rebeccapatek@gmail.com>
2019-12-04 10:35:42 -0600

    added popultion buttons

--- scripts/Vietnam/cities/CityDataProvider.js
+++ scripts/Vietnam/cities/CityDataProvider.js
@@ -1,20 +1,29 @@
-const cityCollection = [
+const citiesCollection = [
     {
-        cityName: "Dalat"
+        cityName: "Dalat",
+        cityPopulation: 103
     },
+    
     {
-        cityName: "Da Nang"
+        cityName: "Da Nang",
+        cityPopulation: 200
     },
+    
     {
-        cityName: "Hoi An"
+        cityName: "Hoi An",
+        cityPopulation: 400000
     },
+    
     { 
-        cityName: "Ho Chi Minh City"
+        cityName: "Ho Chi Minh City",
+        cityPopulation: 500000
     }, 
+    
     {
-        cityName: "Hanoi"
+        cityName: "Hanoi",
+        cityPopultion: 256
     }
 ]
 export const useCity = () => {
-    return cityCollection
+    return citiesCollection
 }
--- scripts/Vietnam/main.js
+++ scripts/Vietnam/main.js
@@ -1,6 +1,8 @@
 import CityListComponent from "./Cities/Cities.js"
 import LandmarkListComponent from "./landmarks/Landmarks.js"
 import LeaderListComponent from "./leaders/Leaders.js"
+import initializeDetailButtonEvents from "./dialog.js"
 CityListComponent()
 LandmarkListComponent()
 LeaderListComponent()
+initializeDetailButtonEvents()
